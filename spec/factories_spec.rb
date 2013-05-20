72eac9d20b5dc65132d51a25ee0897b371dbb3a4
Wojtek Majewski <jumski@gmail.com>
2013-05-20 14:29:28 +0200

    new factories_spec

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
 
 describe "Factories:" do
-  FactoryGirl.factories.map(&:name).each do |factory_name|
-    it 'does not raise any errors' do
-      expect{
-        built = build(factory_name)
+  SKIPPED_TRAITS = []
 
-        if built.respond_to? :valid?
-          expect(built).to be_valid
-        end
-      }.to_not raise_error
+  def self.it_builds(trait = nil)
+    title = "builds"
+    title+= "trait :#{trait}" if trait
+
+    it title do
+      record = build(subject.name, trait)
+
+      if record.respond_to?(:valid?) && !record.valid?
+        msg = "Expected to be valid, but is invalid with errors:" +
+              " #{record.errors.full_messages}"
+
+        fail msg
+      end
     end
+  end
+
+  FactoryGirl.factories.each do |factory|
+    context "#{factory.name} factory" do
+      subject { factory }
+
+      it_builds
+
+      traits = factory.defined_traits.reject do |trait|
+        SKIPPED_TRAITS.include? trait.name
+      end
+
+      traits.each { |trait| it_builds(trait.name) }
+    end
+
   end
 end
