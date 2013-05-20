c386017b75004005836ab238fbfdb82f7d2af318
Wojtek Majewski <jumski@gmail.com>
2013-05-20 14:29:17 +0200

    deal event validations

--- app/models/alle_api/deal_event.rb
+++ app/models/alle_api/deal_event.rb
@@ -1,6 +1,17 @@
 module AlleApi
   class DealEvent < ActiveRecord::Base
-    attr_accessible :occured_at, :quantity, :remote_auction_id, :remote_buyer_id, :remote_deal_id, :remote_id, :remote_seller_id, :remote_transaction_id, :auction
+    NUMERICAL = %w(
+      remote_auction_id remote_buyer_id
+      remote_deal_id remote_id remote_seller_id
+      remote_transaction_id
+    ).map(&:to_sym)
+
+    ACCESSIBLE = [:occured_at, :quantity, :auction] + NUMERICAL
+
+    attr_accessible *ACCESSIBLE
+
+    validates *ACCESSIBLE, presence: true
+    validates *NUMERICAL, numericality: true
 
     belongs_to :auction
 
--- spec/models/alle_api/deal_event_spec.rb
+++ spec/models/alle_api/deal_event_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe AlleApi::DealEvent do
   it { should belong_to :auction }
+
+  AlleApi::DealEvent::ACCESSIBLE.each do |attr|
+    it { should validate_presence_of attr }
+  end
+
+  AlleApi::DealEvent::NUMERICAL.each do |attr|
+    it { should validate_numericality_of attr }
+  end
 end
