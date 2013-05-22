a878ed539f7442cb86921006c7ccf0e1345c8a1b
Wojtek Majewski <jumski@gmail.com>
2013-05-22 15:28:02 +0200

    fetch deal events

---
+++ lib/alle_api/job/fetch_deals_events.rb
@@ -0,0 +1,19 @@
+module AlleApi
+  module Job
+    class FetchDealsEvents < Base
+      sidekiq_options unique: true, unique_job_expiration: 10.minutes
+
+      def perform(account_id)
+        account = AlleApi::Account.find(account_id)
+        api = account.api
+
+        entries = api.get_deals_journal
+        entries.each do |entry|
+          entry.create_deal_event(account)
+        end
+
+        # TriggerAuctionEvents.perform_in(5.seconds, account.id)
+      end
+    end
+  end
+end
---
+++ spec/lib/alle_api/job/fetch_deals_events_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'rspec/allegro'
+require 'rspec/freezed_time_context'
+require 'rspec/sidekiq_examples'
+
+describe AlleApi::Job::FetchDealsEvents do
+  include_context 'with account'
+  include_context 'stubbed get_journal events'
+  include_context 'silenced logger'
+  include_context 'freezed time'
+
+  subject { described_class.new }
+
+  it_is_an 'unique job', 10.minutes
+
+  it { should be_a AlleApi::Job::Base }
+
+  it 'calls api.get_deals_journal' do
+    api.expects(:get_deals_journal).returns([])
+
+    expect(subject.perform(account.id)).to eq []
+  end
+
+  it 'creates new AuctionEvent for each returned event' do
+    event_a, event_b = mock('event a'), mock('event b')
+    api.stubs(get_deals_journal: [event_a, event_b])
+
+    event_a.expects(:create_deal_event).with(account)
+    event_b.expects(:create_deal_event).with(account)
+
+    subject.perform(account.id)
+  end
+
+#   it 'enqueues TriggerAuctionEvents' do
+#     api.stubs(get_journal: [])
+#     subject.perform(account.id)
+#
+#     expect(AlleApi::Job::TriggerAuctionEvents).to \
+#       have_queued_job_at(time_now + 5.seconds, account.id)
+#   end
+end
