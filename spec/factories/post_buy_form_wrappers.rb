424430ad6e7a463582f570bc40b7acee1282f5ed
Wojtek Majewski <jumski@gmail.com>
2013-05-23 11:30:27 +0200

    stubs

---
+++ spec/factories/post_buy_form_wrappers.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+FactoryGirl.define do
+  # factory :post_buy_form_wrapper, class: 'AlleApi::Wrapper::PostBuyForm' do
+  #   skip_create
+
+  #   remote_id 243626480
+  #   source ({some: :hash})
+  #   shipment_id 1
+
+  #   buyer_id 5697909
+  #   buyer_login 'some_login'
+  #   buyer_email 'some@email.com'
+  #   invoice_requested false
+  #   message_to_seller 'some message'
+
+  #   amount 2.0
+  #   postage_amount 1.0
+  #   payment_amount 2
+
+  #   payment_type 'co'
+  #   payment_id 318277336
+  #   payment_status 'Zakończona'
+  #   payment_created_at DateTime.parse('2013-05-21 13:12:40')
+  #   payment_received_at DateTime.parse('2013-05-21 13:12:40')
+  #   payment_cancelled_at nil
+  # end
+end
--- spec/lib/alle_api/wrapper/post_buy_form_spec.rb
+++ spec/lib/alle_api/wrapper/post_buy_form_spec.rb
@@ -3,40 +3,40 @@ require 'spec_helper'
 
 describe AlleApi::Wrapper::PostBuyForm do
 
-  # describe "#create_deal_event" do
-  #   let(:wrapper) { build :new_deal_wrapper }
-  #   let(:account) { create :account }
-  #   let(:auction) do
-  #     create :auction, :published,
-  #       account: account, remote_id: wrapper.remote_auction_id
-  #   end
+  describe "#create_post_buy_form" do
+    # let(:wrapper) { build :new_deal_wrapper }
+    # let(:account) { create :account }
+    # let(:auction) do
+    #   create :auction, :published,
+    #     account: account, remote_id: wrapper.remote_auction_id
+    # end
 
-  #   def create_event!
-  #     auction # trigger create
-  #     wrapper.create_deal_event(account)
-  #   end
-  #   subject { create_event! }
+    # def create_event!
+    #   auction # trigger create
+    #   wrapper.create_deal_event(account)
+    # end
+    # subject { create_event! }
 
-  #   it 'creates new event for first time' do
-  #     expect {
-  #       subject
-  #     }.to change(AlleApi::DealEvent::NewDeal, :count).to(1)
-  #   end
+    # it 'creates new event for first time' do
+    #   expect {
+    #     subject
+    #   }.to change(AlleApi::DealEvent::NewDeal, :count).to(1)
+    # end
 
-  #   it 'is idempotent' do
-  #     expect {
-  #       create_event!
-  #       create_event!
-  #     }.to change(AlleApi::DealEvent::NewDeal, :count).to(1)
-  #   end
+    # it 'is idempotent' do
+    #   expect {
+    #     create_event!
+    #     create_event!
+    #   }.to change(AlleApi::DealEvent::NewDeal, :count).to(1)
+    # end
 
-  #   its(:auction) { should eq auction }
+    # its(:auction) { should eq auction }
 
-  #   attrs = described_class::ATTRIBUTE_NAME_TRANSLATION.values - [:kind]
-  #   attrs.each do |attr|
-  #     its(attr) { should eq wrapper.send(attr) }
-  #   end
-  # end
+    # attrs = described_class::ATTRIBUTE_NAME_TRANSLATION.values - [:kind]
+    # attrs.each do |attr|
+    #   its(attr) { should eq wrapper.send(attr) }
+    # end
+  end
 
   describe "uses wrapper", vcr: 'do_get_post_buy_forms_data_for_sellers', :hax => true do
     include_context 'real api client'
