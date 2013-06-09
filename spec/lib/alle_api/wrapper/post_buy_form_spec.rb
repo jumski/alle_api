2de66a42de16a2e349e283389f41e6f7f533e139
Wojtek Majewski <jumski@gmail.com>
2013-06-09 13:21:39 +0200

    remove dead code

--- spec/lib/alle_api/wrapper/post_buy_form_spec.rb
+++ spec/lib/alle_api/wrapper/post_buy_form_spec.rb
@@ -2,41 +2,6 @@
 require 'spec_helper'
 
 describe AlleApi::Wrapper::PostBuyForm do
-  describe "#create_if_missing" do
-    # let(:wrapper) { build :new_deal_wrapper }
-    # let(:account) { create :account }
-    # let(:auction) do
-    #   create :auction, :published,
-    #     account: account, remote_id: wrapper.remote_auction_id
-    # end
-
-    # def create_event!
-    #   auction # trigger create
-    #   wrapper.create_if_missing(account)
-    # end
-    # subject { create_event! }
-
-    # it 'creates new event for first time' do
-    #   expect {
-    #     subject
-    #   }.to change(AlleApi::DealEvent::NewDeal, :count).to(1)
-    # end
-
-    # it 'is idempotent' do
-    #   expect {
-    #     create_event!
-    #     create_event!
-    #   }.to change(AlleApi::DealEvent::NewDeal, :count).to(1)
-    # end
-
-    # its(:auction) { should eq auction }
-
-    # attrs = described_class::ATTRIBUTE_NAME_TRANSLATION.values - [:kind]
-    # attrs.each do |attr|
-    #   its(attr) { should eq wrapper.send(attr) }
-    # end
-  end
-
   describe "#message_to_seller" do
     it 'is nil if xsi:type string was passed' do
       subject.message_to_seller = "---\n:@xsi:type: xsd:string\n"
