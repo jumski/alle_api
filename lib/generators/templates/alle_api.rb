785de6b647d45da730528229f0cc2e09c1c91944
Wojtek Majewski <jumski@gmail.com>
2016-08-23 12:52:14 +0200

    Improve initializer with all event handlers

--- lib/generators/templates/alle_api.rb
+++ lib/generators/templates/alle_api.rb
@@ -1,10 +1,18 @@
 
 AlleApi.config.tap do |config|
-  # = Description renderer
-  # you can use any object that responds to `call`
+  # Used to render auction's description html.
+  # You can use any object that responds to `call`,
   # it will be called with auction and auctionable as arguments
-  # it's return value will be used as an auction description when published
   config.description_renderer = lambda do |auction, auctionable|
     %Q{ Auction "#{auction.title}" of auctionable with id #{auctionable.id} }
   end
+
+  # Event handlers, called when particular events in auction lifecycle occurs.
+  # You can use any object that responds to `call` and accepts `auction` and `auctionable` arguments.
+  #
+  # config.auction_queued_for_publication_handler = lambda { |auction, auctionable| }
+  # config.auction_queued_for_finishing_handler = lambda { |auction, auctionable| }
+  # config.auction_buy_now_handler = lambda { |auction, auctionable| }
+  # config.auction_published_handler = lambda { |auction, auctionable| }
+  # config.auction_ended_handler = lambda { |auction, auctionable| }
 end
