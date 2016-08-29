9e3b7c7348197985bc0a34eba711a1fc45f5e34a
Wojtek Majewski <jumski@gmail.com>
2016-08-29 17:10:42 +0200

    Increase timeouts

--- spec/http/create_and_finish_auction_spec.rb
+++ spec/http/create_and_finish_auction_spec.rb
@@ -3,18 +3,20 @@ require 'spec_helper'
 require 'rspec/allegro'
 
 describe 'Happy Paths: create and finish auction', :http do
+  before { VCR.eject_cassette }
 
   include_context 'authenticated and updated api client'
   include_context 'real category'
   include_context 'allow http connections'
 
-  before do
-    @auction = create(:auction, account: account)
-    @auction.auctionable.stubs(category_id_for_auction: category.id)
+  let!(:auction) do
+    create(:auction, account: account).tap do |auction|
+      auction.auctionable.stubs(category_id_for_auction: category.id)
+    end
   end
 
   it 'uses full stack to update, authenticate, create and finish' do
-    created_auction = api.create_auction(@auction.to_allegro_auction)
+    created_auction = api.create_auction(auction.to_allegro_auction)
     remote_id = created_auction[:item_id].to_i
     expect(remote_id).to be > 0
 
@@ -29,14 +31,14 @@ describe 'Happy Paths: create and finish auction', :http do
   private
 
   def fetch_journal_entry(remote_id)
-    wait_with_retry(for: 'fetching journal with just ended auction', seconds: 1, times: 90) do
+    wait_with_retry(for: 'fetching journal with just ended auction', seconds: 1, times: 120) do
       journal = api.get_journal(0)
       journal.find { |j| j.remote_auction_id == remote_id } or raise
     end
   end
 
   def finish_auction(remote_id)
-    wait_with_retry(for: 'ending just created auction', seconds: 1, times: 90) do
+    wait_with_retry(for: 'ending just created auction', seconds: 1, times: 120) do
       results = api.finish_auctions([remote_id])
 
       results[:finished].first.tap do |id|
