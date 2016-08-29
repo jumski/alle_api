4d89abe490872f64324e7470188999b5f9d8df67
Wojtek Majewski <jumski@gmail.com>
2016-08-29 17:53:25 +0200

    Rely on AlleApi.config, record wsdls

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,11 +5,11 @@ VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = 'spec/support/cassettes'
   config.hook_into :webmock
-  config.default_cassette_options = { :record => :once }
+  config.default_cassette_options = { :record => :new_episodes }
 
-  config.filter_sensitive_data('{webapi_key}')      { ENV.fetch('ALLE_API_WEBAPI_KEY') }
-  config.filter_sensitive_data('{webapi_login}')    { ENV.fetch('ALLE_API_WEBAPI_LOGIN') }
-  config.filter_sensitive_data('{webapi_password}') { ENV.fetch('ALLE_API_WEBAPI_PASSWORD') }
+  config.filter_sensitive_data('{webapi_key}')      { AlleApi.config.webapi_key }
+  config.filter_sensitive_data('{webapi_login}')    { AlleApi.config.login }
+  config.filter_sensitive_data('{webapi_password}') { AlleApi.config.password }
 
   if ENV['VCR_DEBUG'].present?
     config.debug_logger = File.open('log/vcr.log', 'w')
@@ -17,6 +17,19 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
+  # wrap all specs with WSDL cassette
+  config.around(:each) do |example|
+    cassette = if AlleApi.config.sandbox
+                 :sandbox_wsdl
+               else
+                 :wsdl
+               end
+
+    VCR.use_cassette(cassette, match_requests_on: [:uri, :body]) do
+      example.run
+    end
+  end
+
   config.around(:each) do |example|
     if example.metadata[:vcr].present?
       cassette = example.metadata[:vcr]
